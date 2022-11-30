93522e49bf977f679f4eaea3d27ee084898af3cb
Aimee <961745931@qq.com>
2022-11-30 11:30:41 +0800

    20221123 fail version, abandon

---
+++ 20221123/TraningModel2/datasets_my.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# _*_ coding: UTF-8 _*_
+"""=================================================
+@Project -> File    : 20221123 -> datasets.py
+@IDE     : PyCharm
+@Author  : Aimee
+@Date    : 2022/11/28 13:27
+@Desc    :
+================================================="""
+from torch.utils.data import Dataset
+import os
+from torchvision import transforms
+from PIL import Image
+from torch import zeros, argmax
+from CommonGetPic import captcha_array,captcha_size
+from torch.utils.data import DataLoader
+
+
+def text2Vec(text):
+    vec = zeros(captcha_size,len(captcha_array))
+    # print(vec)
+    for i in range(len(text)):
+        # print(captcha_array.index(text[i]))
+        vec[i, captcha_array.index(text[i])] = 1
+    return vec
+
+
+def vec2Text(vec):
+    vec = argmax(vec, dim=1)
+    # print(vec)
+    text = ""
+    for i in vec:
+        text += captcha_array[i]
+    return text
+
+
+class my_dataset(Dataset):
+    def __init__(self, root_dir):
+        super(my_dataset, self)
+        self.image_path = [os.path.join(root_dir,image_name) for image_name in os.listdir(root_dir)]
+        # Resize那里是图片的像素大小，可右键图片查看
+        self.transfroms = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Resize((50, 300)),
+                transforms.Grayscale()
+            ]
+        )
+        print(self.image_path)
+
+    def __len__(self):
+        return self.image_path.__len__()
+
+    def __getitem__(self, index):
+        image_path = self.image_path[index]
+        # 将图片灰度化
+        image = self.transfroms(Image.open(image_path))
+        # image = Image.open(image_path)
+        # image.show()
+        label = image_path.split("/")[-1]
+        # 取出验证码的字符
+        label = label.split("_")[0]
+        label_tensor = text2Vec(label)
+        label_tensor = label_tensor.view(1,-1)[0]
+        return image,label_tensor
+
+
+def main():
+    test_dataset = my_dataset("./datasets/test/")
+    test_dataloader = DataLoader(test_dataset, batch_size=40,shuffle=True)
+    for i,(images,labels) in enumerate(test_dataloader):
+        print(images.shape)
+        print(labels.shape)
+
+
+if __name__ == '__main__':
+    main()
---
+++ 20221123/TraningModel2/train.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# _*_ coding: UTF-8 _*_
+"""=================================================
+@Project -> File    : 20221123 -> train.py
+@IDE     : PyCharm
+@Author  : Aimee
+@Date    : 2022/11/28 13:30
+@Desc    :
+================================================="""
+from torch.utils.data import DataLoader
+from datasets_my import my_dataset
+from torch import nn
+from torch.optim import Adam
+from VKModel import vkmodel
+from torch.utils.tensorboard import SummaryWriter
+import torch
+
+
+def main():
+
+    train_dataset = my_dataset("./datasets/train/")
+    train_dataloader = DataLoader(train_dataset, batch_size=40, shuffle=True)
+    # vk_model = vkmodel()  # CPU
+    vk_model = vkmodel().cuda()  # GPU
+    # loss_fn = nn.MultiLabelSoftMarginLoss()  # CPU
+    loss_fn = nn.MultiLabelSoftMarginLoss().cuda()  # GPU
+    optim = Adam(vk_model.parameters(), lr=0.001)
+    # w = SummaryWriter("logs")
+    total_step = 0
+    for epoch in range(10):
+        print("外层训练次数{}".format(epoch))
+        for i, (images, labels) in enumerate(train_dataloader):
+            images = images.cuda()  # GPU
+            labels = labels.cuda()  # GPU
+            vk_model.train()
+            outputs = vk_model(images)
+            loss = loss_fn(outputs, labels)
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+            total_step += 1
+            if i % 100 == 0:
+                print("训练次数{},损失率{}".format(i, loss.item()))
+                # w.add_scalar("loss",loss,total_step)
+    torch.save(vk_model, "model.pth")
+
+
+if __name__ == '__main__':
+    main()
